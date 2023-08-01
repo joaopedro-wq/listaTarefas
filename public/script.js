ac8492aac639c20af248617e07398cd5cd8043a6
joaopedro-wq <joao.bandeiraoliveira@gmail.com>
2023-08-01 05:24:41 -0300

    first commit

---
+++ public/script.js
@@ -0,0 +1,238 @@
+const formularioTarefa = document.getElementById('formularioTarefa');
+const listaTarefas = document.getElementById('listaTarefas');
+const formEditarTarefa = document.getElementById('formEditarTarefa');
+const formEditar = document.getElementById('formEditar');
+let draggedIndex; 
+
+
+function excluirTarefa(idTarefa) {
+  
+  const confirmacao = window.confirm('Tem certeza que deseja excluir esta tarefa?');
+  if (confirmacao) {
+    axios.delete(`http://localhost:3000/api/tarefas/${idTarefa}`)
+      .then(response => {
+        console.log('Tarefa excluída com sucesso:', response.data);
+        carregarTarefas(); 
+      })
+      .catch(error => {
+        console.error('Erro ao excluir a tarefa:', error);
+      });
+  } else {
+    
+    console.log('Exclusão cancelada pelo usuário.');
+  }
+}
+
+function dragStart(event) {
+  draggedIndex = parseInt(event.target.dataset.index, 10);
+  event.dataTransfer.effectAllowed = 'move';
+  event.dataTransfer.setData('text/plain', draggedIndex);
+}
+
+function dragOver(event) {
+  event.preventDefault();
+}
+
+function drop(event) {
+  event.preventDefault();
+  const targetIndex = parseInt(event.target.dataset.index, 10);
+  if (draggedIndex !== targetIndex) {
+    const listaTarefas = document.getElementById('listaTarefas');
+    const tarefas = Array.from(listaTarefas.children);
+    const draggedTask = tarefas[draggedIndex];
+    const targetTask = tarefas[targetIndex];
+    listaTarefas.insertBefore(draggedTask, targetTask);
+    
+  }
+}
+
+function carregarTarefas() {
+  axios.get('http://localhost:3000/api/tarefas')
+    .then(response => {
+      const tarefas = response.data;
+      listaTarefas.innerHTML = '';
+      tarefas.forEach((tarefa, index) => { 
+        const li = document.createElement('li');
+        li.dataset.index = index;
+        li.draggable = true;
+        li.ondragstart = dragStart;
+        li.ondragover = dragOver;
+        li.ondrop = drop;
+        li.innerHTML = `
+        <span class="drag-handle"> ${tarefa.id}</span>
+         
+          <span>${tarefa.nome}</span>
+          <span>${tarefa.custo.toFixed(2)}</span>
+          <span>${formatarData(tarefa.data_limite)}</span>
+         
+          <div class="botoes-tarefa">
+          <button id="editar-btn" onclick="exibirFormEditar(${tarefa.id}, '${tarefa.nome}', ${tarefa.custo}, '${tarefa.dataLimite}')">
+          <i class="fas fa-pencil-alt"></i> Editar
+        </button>
+        <button id="excluir-btn" onclick="excluirTarefa(${tarefa.id})">
+          <i class="fas fa-trash"></i> Excluir
+        </button>
+          </div>
+
+         
+        `;
+
+        if (tarefa.custo >= 1000) {
+          li.classList.add('tarefa-custo-alto');
+        }
+
+
+        listaTarefas.appendChild(li);
+      });
+    })
+    .catch(error => {
+      console.error('Erro ao obter as tarefas:', error);
+    });  
+}
+
+
+
+
+function adicionarTarefa(event) {
+  event.preventDefault();
+  const nomeTarefa = document.getElementById('nomeTarefa').value;
+  const custoTarefa = parseFloat(document.getElementById('custoTarefa').value);
+  const dataLimiteTarefa = document.getElementById('dataLimiteTarefa').value;
+
+  // Verificar se já existe uma tarefa com o mesmo nome
+  axios.get('http://localhost:3000/api/tarefas')
+    .then(response => {
+      const tarefas = response.data;
+      const tarefaExistente = tarefas.find(tarefa => tarefa.nome === nomeTarefa);
+      if (tarefaExistente) {
+        const mensagem = `Já existe uma tarefa com o mesmo nome: ${nomeTarefa}`;
+        exibirMensagemErro(mensagem);
+        return;
+      }
+
+      // Remove a propriedade ordemApresentacaoTarefa do objeto de dados da tarefa
+      const dadosTarefa = {
+        nome: nomeTarefa,
+        custo: custoTarefa,
+        dataLimite: dataLimiteTarefa,
+      };
+
+      axios.post('http://localhost:3000/api/tarefas', dadosTarefa)
+        .then(response => {
+          console.log('Tarefa adicionada com sucesso:', response.data);
+          carregarTarefas();
+        })
+        .catch(error => {
+          console.error('Erro ao adicionar a tarefa:', error);
+        });
+    })
+    .catch(error => {
+      console.error('Erro ao obter as tarefas:', error);
+    });
+}
+
+function exibirMensagemErro(mensagem) {
+  const mensagemErro = document.getElementById('mensagemErro');
+  mensagemErro.textContent = mensagem;
+  mensagemErro.style.display = 'block';
+
+  
+  setTimeout(() => {
+    mensagemErro.style.display = 'none';
+  }, 5000); 
+}
+
+function formatarData(dataString) {
+  const data = new Date(dataString);
+  const dia = String(data.getDate()).padStart(2, '0');
+  const mes = String(data.getMonth() + 1).padStart(2, '0');
+  const ano = data.getFullYear();
+  return `${dia}/${mes}/${ano}`;
+}
+function exibirFormEditar(idTarefa, nomeTarefa, custoTarefa, dataLimiteTarefa) {
+  
+  document.getElementById('editarNomeTarefa').value = nomeTarefa;
+  document.getElementById('editarCustoTarefa').value = custoTarefa;
+  document.getElementById('editarDataLimiteTarefa').value = dataLimiteTarefa;
+
+  
+  document.getElementById('idTarefaEditar').value = idTarefa;
+
+  
+  formEditarTarefa.style.display = 'block';
+
+  
+  formEditar.onsubmit = function(event) {
+    event.preventDefault();
+    
+    const novoNomeTarefa = document.getElementById('editarNomeTarefa').value;
+    const novoCustoTarefa = parseFloat(document.getElementById('editarCustoTarefa').value);
+    const novaDataLimiteTarefa = document.getElementById('editarDataLimiteTarefa').value;
+
+   
+    axios.get('http://localhost:3000/api/tarefas')
+      .then(response => {
+        const tarefas = response.data;
+        const tarefaExistente = tarefas.find(tarefa => tarefa.nome === novoNomeTarefa && tarefa.id !== idTarefa);
+        if (tarefaExistente) {
+          const mensagem = `Já existe uma tarefa com o mesmo nome: ${novoNomeTarefa}`;
+          exibirMensagemErro(mensagem);
+          return;
+        }
+
+        
+        const dadosTarefa = {
+          nome: novoNomeTarefa,
+          custo: novoCustoTarefa,
+          dataLimite: novaDataLimiteTarefa
+        };
+
+        
+        atualizarTarefa(idTarefa, dadosTarefa);
+
+        
+        formEditarTarefa.style.display = 'none';
+      })
+      .catch(error => {
+        console.error('Erro ao obter as tarefas:', error);
+      });
+  };
+}
+
+
+function editarTarefa(idTarefa) {
+  console.log('Exibindo formulário de edição para a tarefa de ID:', idTarefa);
+
+ 
+  axios.get(`http://localhost:3000/api/tarefas/${idTarefa}`)
+    .then(response => {
+      const tarefa = response.data;
+      exibirFormEditar(tarefa.id, tarefa.nome, tarefa.custo, tarefa.dataLimite, tarefa.ordemApresentacao);
+    })
+    .catch(error => {
+      console.error('Erro ao obter os dados da tarefa:', error);
+    });
+}
+
+
+
+function atualizarTarefa(idTarefa, dadosTarefa) {
+  axios.put(`http://localhost:3000/api/tarefas/${idTarefa}`, dadosTarefa)
+    .then(response => {
+      console.log('Tarefa atualizada com sucesso:', response.data);
+      carregarTarefas();
+    })
+    .catch(error => {
+      console.error('Erro ao atualizar a tarefa:', error);
+    });
+}
+
+
+function fecharFormEditar() {
+  formEditarTarefa.style.display = 'none';
+}
+
+
+
+formularioTarefa.addEventListener('submit', adicionarTarefa);
+carregarTarefas();
---
+++ server.js
@@ -0,0 +1,95 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('tarefas.db');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Definir cabeçalhos para evitar problemas de CORS (Cross-Origin Resource Sharing)
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  next();
+});
+
+// Definir rota para obter todas as tarefas
+app.get('/api/tarefas', (req, res) => {
+  db.all('SELECT * FROM tarefas ORDER BY ordem_apresentacao', (err, rows) => {
+    if (err) {
+      console.error(err.message);
+      res.status(500).json({ error: 'Erro ao buscar as tarefas' });
+    } else {
+      res.json(rows);
+    }
+  });
+});
+
+
+// Definir rota para adicionar uma nova tarefa
+app.post('/api/tarefas', (req, res) => {
+  const { nome, custo, dataLimite } = req.body;
+
+  // Obter o número atual de tarefas na tabela
+  db.get('SELECT COUNT(*) as total FROM tarefas', (err, row) => {
+    if (err) {
+      console.error(err.message);
+      res.status(500).json({ error: 'Erro ao adicionar a tarefa' });
+    } else {
+      const ordemApresentacao = row.total + 1; // Incrementar 1 para a nova tarefa
+      const query = 'INSERT INTO tarefas (nome, custo, data_limite, ordem_apresentacao) VALUES (?, ?, ?, ?)';
+      db.run(query, [nome, custo, dataLimite, ordemApresentacao], function (err) {
+        if (err) {
+          console.error(err.message);
+          res.status(500).json({ error: 'Erro ao adicionar a tarefa' });
+        } else {
+          res.json({ id: this.lastID });
+        }
+      });
+    }
+  });
+});
+
+
+// Definir rota para atualizar uma tarefa pelo ID
+app.put('/api/tarefas/:id', (req, res) => {
+  const idTarefa = req.params.id;
+  const { nome, custo, dataLimite, ordemApresentacao } = req.body;
+  const query = 'UPDATE tarefas SET nome = ?, custo = ?, data_limite = ? WHERE id = ?';
+
+
+  db.run(query, [nome, custo, dataLimite, idTarefa], function (err) {
+    if (err) {
+      console.error(err.message);
+      res.status(500).json({ error: 'Erro ao atualizar a tarefa' });
+    } else {
+      res.json({ message: 'Tarefa atualizada com sucesso' });
+    }
+  });
+});
+
+
+// Definir rota para excluir uma tarefa pelo ID
+app.delete('/api/tarefas/:id', (req, res) => {
+  const idTarefa = req.params.id;
+  const query = 'DELETE FROM tarefas WHERE id = ?';
+  db.run(query, [idTarefa], function (err) {
+    if (err) {
+      console.error(err.message);
+      res.status(500).json({ error: 'Erro ao excluir a tarefa' });
+    } else {
+      res.json({ message: 'Tarefa excluída com sucesso' });
+    }
+  });
+});
+
+// Iniciar o servidor
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
+
+
